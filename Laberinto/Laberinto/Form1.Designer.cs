19519f290780cc587db1c994e2acf10517b273bd
Andrés Montoya <84055444+Montoya086@users.noreply.github.com>
2022-05-01 14:18:02 -0600

    Laberinto

---
+++ Laberinto/Laberinto/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Laberinto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Abrir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Resolver";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(240, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1016, 697);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(74, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(51, 43);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "W";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(74, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(51, 43);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "S";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(131, 72);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(51, 43);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "D";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(17, 72);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(51, 43);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "A";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 180);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 143);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gamepad";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1259, 721);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Laberinto/Laberinto/Form1.cs
@@ -0,0 +1,359 @@
+﻿// Andrés Montoya
+// C5E
+// 26/05/2020
+// 29/05/2020
+// Laberinto
+// Programa para la creacion y resolucion de un laberinto
+// Utilizacion de recursion, ciclos y archivos secuenciales
+// C#
+// .NET Framework
+// Visual Studio
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Laberinto
+{
+    public partial class Form1 : Form
+    {
+        string ruta;
+        char[,] mat = new char[50, 50];
+        Graphics canvas;
+        Label l = new Label();
+        int x1, y1;
+        Bitmap bmp;
+
+        public Form1()
+        {
+            InitializeComponent();     
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            canvas = Graphics.FromImage(bmp);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            this.Controls.Add(l);
+            l.Location = pictureBox1.Location;
+            l.Font = new Font("Webdings", 7);
+            l.Text = "b";
+            l.Height = 15;
+            l.Width = 15;
+            l.BackColor = Color.White;
+            bool bandera = false;
+            int x = 0, y = 0;
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            abrir.Title = "Abrir";
+            if (abrir.ShowDialog() == DialogResult.OK)
+            {
+                canvas.Clear(Color.White);
+                ruta = abrir.FileName;
+                using (StreamReader sr = new StreamReader(ruta))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string s = sr.ReadLine();
+                        for (x = 0; x < s.Length; x++)
+                        {
+                            mat[x, y] = s[x];
+                            switch (s[x])
+                            {
+                                case ' ':
+                                    break;
+                                case 'E':
+                                    canvas.DrawString("D", new Font("Webdings", 10), Brushes.Black, x * 15, y * 15);
+                                    break;
+                                default:
+                                    canvas.FillRectangle(Brushes.Black, x * 15, y * 15, 15, 15);
+                                    break;
+                            }
+                            if (mat[x, y] == ' ' && bandera == false)
+                            {
+                                bandera = true;
+                                l.Location = new Point(15 * x + pictureBox1.Left, 15 * y + pictureBox1.Top);
+                                l.BringToFront();
+                                x1 = x; y1 = y;
+                            }
+                        }
+                        y++;
+                    }
+                    sr.Close();
+                    pictureBox1.Image = bmp;
+                }
+            }
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.D:
+
+                    if (pictureBox1.CanFocus)
+                    {
+                        pictureBox1.Focus();
+                    }
+                    break;
+                case Keys.W:
+
+                    if (pictureBox1.CanFocus)
+                    {
+                        pictureBox1.Focus();
+                    }
+                    break;
+                case Keys.A:
+
+                    if (pictureBox1.CanFocus)
+                    {
+                        pictureBox1.Focus();
+                    }
+                    break;
+                case Keys.S:
+
+                    if (pictureBox1.CanFocus)
+                    {
+                        pictureBox1.Focus();
+                    }
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (mat[x1, y1 - 1] == ' ' || mat[x1 + 1, y1 - 1] == 'E')
+            {
+                l.Top -= 15;
+                y1--;
+                if (mat[x1, y1 - 1] == 'E')
+                {
+                    MessageBox.Show("Ganaste we");
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (mat[x1 - 1, y1] == ' ' || mat[x1 - 1, y1] == 'E')
+            {
+                l.Left -= 15;
+                x1--;
+                if (mat[x1 - 1, y1] == 'E')
+                {
+                    MessageBox.Show("Ganaste we");
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (mat[x1, y1 + 1] == ' ' || mat[x1, y1 + 1] == 'E')
+            {
+                l.Top += 15;
+                y1++;
+                if (mat[x1, y1 + 1] == 'E')
+                {
+                    MessageBox.Show("Ganaste we");
+                }
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (mat[x1 + 1, y1] == ' ' || mat[x1 + 1, y1] == 'E')
+            {
+                l.Left += 15;
+                x1++;
+                if (mat[x1 + 1, y1] == 'E')
+                {
+                    MessageBox.Show("Ganaste we");
+                }
+            }
+        }
+        bool bandera = true;
+        public void resolver(int x, int y)
+        {
+            switch (mat[x, y])
+            {
+                case ' ':
+                    mat[x, y] = '.';
+                    mover(x, y);
+                    resolver(x, y + 1);
+                    resolver(x, y - 1);
+                    resolver(x-1, y );
+                    resolver(x+1, y );
+                    mover(x, y);
+                    break;
+                case 'E':
+                    bandera = false;
+                    MessageBox.Show("Tramposo ¬¬");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        public void mover(int x, int y)
+            {
+            if (bandera)
+            {
+                l.Left = pictureBox1.Left + x * 15;
+                l.Top = pictureBox1.Top + y * 15;
+                Application.DoEvents();
+                Thread.Sleep(70);
+            }
+            }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resolver(x1, y1);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.D:
+                    if (mat[x1+1,y1]==' ' || mat[x1 + 1, y1] == 'E')
+                    {
+                        l.Left += 15;
+                        x1++;
+                        if (mat[x1 + 1, y1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if(button5.CanFocus)
+                    {
+                        button5.Focus();
+                    }
+                    break;
+                case Keys.W:
+                    if (mat[x1, y1-1] == ' ' || mat[x1 + 1, y1-1] == 'E')
+                    {
+                        l.Top -= 15;
+                        y1--;
+                        if (mat[x1, y1-1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button3.CanFocus)
+                    {
+                        button3.Focus();
+                    }
+                    break;
+                case Keys.A:
+                    if (mat[x1 - 1, y1] == ' ' || mat[x1 - 1, y1] == 'E')
+                    {
+                        l.Left -= 15;
+                        x1--;
+                        if (mat[x1 - 1, y1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button6.CanFocus)
+                    {
+                        button6.Focus();
+                    }
+                    break;
+                case Keys.S:
+                    if (mat[x1, y1+1] == ' ' || mat[x1, y1+1] == 'E')
+                    {
+                        l.Top += 15;
+                        y1++;
+                        if (mat[x1, y1+1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button4.CanFocus)
+                    {
+                        button4.Focus();
+                    }
+                    break;
+                    //flechitas
+                case Keys.Right:
+                    if (mat[x1 + 1, y1] == ' ' || mat[x1 + 1, y1] == 'E')
+                    {
+                        l.Left += 15;
+                        x1++;
+                        if (mat[x1 + 1, y1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button5.CanFocus)
+                    {
+                        button5.Focus();
+                    }
+                    break;
+                case Keys.Up:
+                    if (mat[x1, y1 - 1] == ' ' || mat[x1 + 1, y1 - 1] == 'E')
+                    {
+                        l.Top -= 15;
+                        y1--;
+                        if (mat[x1, y1 - 1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button3.CanFocus)
+                    {
+                        button3.Focus();
+                    }
+                    break;
+                case Keys.Left:
+                    if (mat[x1 - 1, y1] == ' ' || mat[x1 - 1, y1] == 'E')
+                    {
+                        l.Left -= 15;
+                        x1--;
+                        if (mat[x1 - 1, y1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button6.CanFocus)
+                    {
+                        button6.Focus();
+                    }
+                    break;
+                case Keys.Down:
+                    if (mat[x1, y1 + 1] == ' ' || mat[x1, y1 + 1] == 'E')
+                    {
+                        l.Top += 15;
+                        y1++;
+                        if (mat[x1, y1 + 1] == 'E')
+                        {
+                            MessageBox.Show("Ganaste we");
+                        }
+                    }
+                    if (button4.CanFocus)
+                    {
+                        button4.Focus();
+                    }
+                    break;
+            }
+        }
+    }
+}
